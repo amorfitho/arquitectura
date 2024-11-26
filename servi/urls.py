2d0299d9621db09a79e2cc9d5f42a09b99f5083b
Fabian <119624256+amorfitho@users.noreply.github.com>
2024-11-26 20:18:17 -0300

    configulacion rutas de pagina

--- servi/urls.py
+++ servi/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import main
+from .views import main, login, registro, reservas, reservas2,servicios, productos, empleados, horarios
 
 urlpatterns = [
     path('', main, name="main"),
+    path('login/', login, name="login"),
+    path('registro/', registro, name="registro"),
+    path('reservas/', reservas, name="reservas"),
+    path('reservas2/', reservas2, name="reservas2"),
+    path('servicios/', servicios, name="servicios"),
+    path('productos/', productos, name="productos"),
+    path('empleados/', empleados, name="empleados"),
+    path('horarios/', horarios, name="horarios"),
+
 ]
--- servi/views.py
+++ servi/views.py
@@ -2,5 +2,33 @@ from django.shortcuts import render
 
 # Create your views here.
 
+#
 def main(request):
     return render(request, 'servi/main.html')
+#
+def login(request):
+    return render(request, 'servi/login.html')
+#
+def regsitro(request):
+    return render(request, 'servi/registro.html')
+#
+def reservas(request):
+    return render(request, 'servi/reservas.html')
+#
+def reservas2(request):
+    return render(request, 'servi/reservas2.html')
+#
+def servicios(request):
+    return render(request, 'servi/servicios.html')
+#
+def productos(request):
+    return render(request, 'servi/productos.html')
+#
+def empleados(request):
+    return render(request, 'servi/empleados.html')
+#
+def horarios(request):
+    return render(request, 'servi/horarios.html')
+#
+#
+#
