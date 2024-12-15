71e1403732b0ad7247f3b72e3af8543f5d64c227
elmatimene <ma.menesesu@duocuc.cl>
2024-12-15 19:12:39 -0300

    intente hacer el listado de las reservas y hise el diseño del  registro

--- servi/forms.py
+++ servi/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from.models import Cita
 
 class CitaForm(forms.ModelForm):
+
+    #nombre = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+
     class Meta:
         model = Cita
         fields =["nombre","email","modelo","patente","marca","servicio","fecha","precio"]
--- serviExpess/settings.py
+++ serviExpess/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'servi',
 ]
 
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
